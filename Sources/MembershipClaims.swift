6ed7a758057a5b03989f0bbd6037ca6b71c63dc1
Fabio Tacke <f.tacke@ticeapp.com>
2021-04-28 15:37:00 +0200

    Drop support for deprecated issuer serialization

--- Sources/MembershipClaims.swift
+++ Sources/MembershipClaims.swift
@@ -52,29 +52,15 @@ public struct MembershipClaims: JWTPayload {
         }
 
         public init(from decoder: Decoder) throws {
-            do {
-                let rawString = try decoder.singleValueContainer().decode(String.self)
-
-                if rawString == "server" {
-                    self = .server
-                } else {
-                    guard let userId = UserId(uuidString: rawString) else {
-                        throw CertificateValidationError.invalidClaims
-                    }
-                    self = .user(userId)
+            let rawString = try decoder.singleValueContainer().decode(String.self)
+            
+            if rawString == "server" {
+                self = .server
+            } else {
+                guard let userId = UserId(uuidString: rawString) else {
+                    throw CertificateValidationError.invalidClaims
                 }
-            } catch is DecodingError {
-                let container = try decoder.container(keyedBy: CodingKeys.self)
-                if let userId = try container.decodeIfPresent(UUID.self, forKey: .user) {
-                    self = .user(userId)
-                } else {
-                    let server = try container.decode(String.self, forKey: .server)
-                    guard server == "server" else {
-                        throw CertificateValidationError.invalidClaims
-                    }
-
-                    self = .server
-                }
+                self = .user(userId)
             }
         }
 
